c9da2376285f055e6016abfedbb5217af1989a85
Danylo Burenkov <danylo.burenkov@gmail.com>
2024-12-11 16:47:55 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,12 @@ export default function RootLayout({
             disableTransitionOnChange
         >
             <Header/>
+            <BackgroundBeamsWithCollision>
                 {children}
                 <div className={"fixed bottom-6 right-6 "}>
                     <ModeToggle/>
                 </div>
+            </BackgroundBeamsWithCollision>
         </ThemeProvider>
         <Analytics/>
         <SpeedInsights/>
