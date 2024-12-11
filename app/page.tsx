7b9258d9294d779ba2ecfd2a7ac593896198749d
Danylo Burenkov <danylo.burenkov@gmail.com>
2024-12-11 12:05:25 +0100

    feat: skills +fixed separator.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import {BlogPosts} from "@/components/posts";
 import Hero from "@/components/hero/hero";
 import {Separator} from "@/components/ui/separator";
 import Projects from "@/components/projects/projects";
+import Skills from "@/components/skills/skills";
 
 export default function Home() {
 
@@ -9,12 +10,10 @@ export default function Home() {
 
         < main className={"flex flex-col items-center px-4 min-h-[1000rem]"}>
             <Hero/>
-            <Separator className={"bg-gray-700 dark:bg-white mt-1 sm:mt-8 w-1/2 mb-8"}/>
+            <Separator className={"bg-gray-700 dark:bg-white mt-1 sm:mt-8 w-1/2 mb-8"} animate={"true"}/>
             <Projects/>
-            {/*<Separator className={"bg-gray-700 dark:bg-white mt-1 sm:mt-8 w-1/2 mb-8"} animate={true}/>*/
-            }
-            {/*<Skills/>*/
-            }
+            <Separator className={"bg-gray-700 dark:bg-white mt-1 sm:mt-8 w-1/2 mb-8"} animate={"true"}/>
+            <Skills/>
             {/*<Experience/>*/
             }
             {/*<Contact/>*/
---
+++ components/skills/skills.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from 'react';
+import SectionHeading from "@/components/ui/section-heading";
+import {skillsData} from "@/lib/data";
+import {useSectionInView} from "@/lib/hooks";
+import {motion} from 'motion/react';
+
+
+const Skills = () => {
+
+    const ref = useSectionInView("Skills");
+    return (
+        <section ref={ref} className={"mb-28 max-w-[53rem] scroll-mt-28 text-center "} id={"skills"}>
+            <SectionHeading>
+                My Skills
+            </SectionHeading>
+            <ul className={"flex flex-wrap justify-center gap-2 text-lg dark:text-gray-800 text-gray-200"}>
+                {
+                    skillsData.map((skill, index) => (
+                        <motion.li key={index} className={"dark:bg-white bg-gray-700 border border-black[0.1] rounded-xl px-5"}
+                                   initial={{scale: 0}}
+                                   whileInView={{scale: 1}}
+                                   transition={{delay: index * 0.1}}
+                                   viewport={{once: true}}
+                        >
+                            {skill}
+                        </motion.li>
+                    ))
+                }
+            </ul>
+        </section>
+    );
+};
+
+export default Skills;
