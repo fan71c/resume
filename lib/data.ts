b5b5d13018ca625630f0e78ac3a2d09064439598
Danylo Burenkov <danylo.burenkov@gmail.com>
2024-12-11 13:16:04 +0100

    feat: experience timeline

--- lib/data.ts
+++ lib/data.ts
@@ -43,14 +43,14 @@ export const experiencesData = [
         icon: React.createElement(FaBriefcase),
         date: "Oct 2023 - Apr 2024",
     },
-    {
-        title: "Master's degree, Computer and Information Technology",
-        location: "University of Pennsylvania, USA",
-        description:
-            "Currently pursuing a Master's degree with a focus on Computer and Information Technology",
-        icon: React.createElement(FaGraduationCap),
-        date: "Jul 2023 - Aug 2025",
-    },
+    // {
+    //     title: "Master's degree, Computer and Information Technology",
+    //     location: "University of Pennsylvania, USA",
+    //     description:
+    //         "Currently pursuing a Master's degree with a focus on Computer and Information Technology",
+    //     icon: React.createElement(FaGraduationCap),
+    //     date: "Jul 2023 - Aug 2025",
+    // },
     {
         title: "Marketing Behavioral Lab Research Assistant",
         location: "Lawrence, Kansas, United States",
@@ -67,21 +67,21 @@ export const experiencesData = [
         date: "Oct 2022 - Apr 2023",
     },
 
-    {
-        title: "Study Abroad - Finance",
-        location: "The London School of Economics and Political Science (LSE), UK",
-        description: "Completed a Finance study abroad program specializing in Alternative Investments",
-        icon: React.createElement(FaGraduationCap),
-        date: "Jun 2022 - Jul 2022",
-    },
-    {
-        title: "Study Abroad - Marketing",
-        location: "CIMBA, Paderno del Grappa, Italy",
-        description:
-            "Studied International Marketing and The Global Consumer, focused on Global Business Education in Italy",
-        icon: React.createElement(FaGraduationCap),
-        date: "May 2022 - Jun 2022",
-    },
+    // {
+    //     title: "Study Abroad - Finance",
+    //     location: "The London School of Economics and Political Science (LSE), UK",
+    //     description: "Completed a Finance study abroad program specializing in Alternative Investments",
+    //     icon: React.createElement(FaGraduationCap),
+    //     date: "Jun 2022 - Jul 2022",
+    // },
+    // {
+    //     title: "Study Abroad - Marketing",
+    //     location: "CIMBA, Paderno del Grappa, Italy",
+    //     description:
+    //         "Studied International Marketing and The Global Consumer, focused on Global Business Education in Italy",
+    //     icon: React.createElement(FaGraduationCap),
+    //     date: "May 2022 - Jun 2022",
+    // },
     {
         title: "Applied Portfolio Management (APM): Student Analyst",
         location: "Lawrence, Kansas, United States",
@@ -89,15 +89,15 @@ export const experiencesData = [
             "Managed a $500K endowment fund with a team, applied market capitalization techniques, and gained hands-on experience from Wall Street professionals",
         icon: React.createElement(FaChartPie),
         date: "Jan 2022 - May 2022",
-    },
-    {
-        title: "Double Bachelor's degree, Finance & Marketing",
-        location: "The University of Kansas School of Business, USA",
-        description:
-            "Graduated with a double major in Finance and Marketing, consistently achieving Honor Roll status with a GPA of 3.9 or higher throughout all semesters",
-        icon: React.createElement(FaGraduationCap),
-        date: "Aug 2020 - May 2023",
     }
+    // {
+    //     title: "Double Bachelor's degree, Finance & Marketing",
+    //     location: "The University of Kansas School of Business, USA",
+    //     description:
+    //         "Graduated with a double major in Finance and Marketing, consistently achieving Honor Roll status with a GPA of 3.9 or higher throughout all semesters",
+    //     icon: React.createElement(FaGraduationCap),
+    //     date: "Aug 2020 - May 2023",
+    // }
 
 ] as const;
 
